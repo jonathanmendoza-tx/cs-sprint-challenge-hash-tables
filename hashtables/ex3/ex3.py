90e6ceac2cb68a5c3c8bf7c14a34fb97f0fcc1ca
Jonathan Mendoza <jon_94us@yahoo.com>
2020-09-13 22:33:22 -0500

    ex3 complete, passing tests

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,17 +1,37 @@
 def intersection(arrays):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
 
-    return result
+	length = len(arrays)
+
+	num_dict = {}
+
+	result = []
+
+	for num_list in arrays:
+
+		for num in num_list:
+
+			if num in num_dict:
+
+				num_dict[num] += 1
+
+			else:
+
+				num_dict[num] = 1
+
+	for nums in num_dict.items():
+
+		if nums[1] == length:
+
+			result.append(nums[0])
+
+	return result
 
 
 if __name__ == "__main__":
-    arrays = []
+	arrays = []
 
-    arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
-    arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
-    arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
+	arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
+	arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
+	arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
 
-    print(intersection(arrays))
+	print(intersection(arrays))
