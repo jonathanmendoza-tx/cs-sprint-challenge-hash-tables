fd391d1ed425645a5b0099d64e643437fc4bd676
Jonathan Mendoza <jon_94us@yahoo.com>
2020-09-13 21:21:31 -0500

    Ex1 completed, tests passed

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,40 @@
 def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
 
-    return None
+	weights_dict = {}
+
+	answer = ()
+
+	for i in range(length):
+
+		if weights[i] in weights_dict:
+
+			weights_dict[weights[i]].add(i)
+
+		else:
+
+			weights_dict[weights[i]] = set()
+			weights_dict[weights[i]].add(i)
+
+	for num in weights_dict.items():
+
+		index = num[1].pop()
+
+		remainder = limit - num[0]
+
+		if remainder in weights_dict:
+
+			print(weights_dict[remainder])
+
+			answer = (index, weights_dict[remainder].pop())
+
+			return sorted(answer, reverse = True)
+
+		else:
+
+			num[1].add(index)
+
+	return None
+
+	
+
+	
