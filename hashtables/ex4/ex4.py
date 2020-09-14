3b6315f5bfb3f9f8d2ec9eb31a14719250c236d8
Jonathan Mendoza <jon_94us@yahoo.com>
2020-09-13 22:56:36 -0500

    ex4 completed, passing tests

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,11 +1,28 @@
 def has_negatives(a):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
 
-    return result
+	num_dict = {}
+
+	result = []
+
+	for num in a:
+
+		if num in num_dict:
+
+			num_dict[num] += 1
+
+		else:
+			num_dict[num] = 1
+
+	for num in num_dict.keys():
+
+		if num > 0:
+
+			if -num in num_dict:
+
+				result.append(num)
+
+	return result
 
 
 if __name__ == "__main__":
-    print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
+	print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
